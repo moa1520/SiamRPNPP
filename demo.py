1b7cc16176a61fe149a863f132ff0c304b2f61f4
taekyung kang <douit_be@naver.com>
2020-08-06 03:30:04 +0900

    selectROI

--- demo.py
+++ demo.py
@@ -1,6 +1,41 @@
+import cv2
 import torch
+import numpy as np
+from glob import glob
+
 from model import ModelBuilder
-from tracker import build_tracker
+# from tracker import build_tracker
+
+
+def get_frames(video_name):
+    if not video_name:
+        cap = cv2.VideoCapture(0)
+        # warmup
+        for i in range(5):
+            cap.read()
+        while True:
+            ret, frame = cap.read()
+            if ret:
+                yield frame
+            else:
+                break
+    elif video_name.endswith('avi') or \
+            video_name.endswith('mp4'):
+        cap = cv2.VideoCapture(video_name)
+        while True:
+            ret, frame = cap.read()
+            if ret:
+                yield frame
+            else:
+                break
+    else:
+        images = glob(os.path.join(video_name, '*.jp*'))
+        images = sorted(images,
+                        key=lambda x: x.split('/')[-1].split('.')[0])
+        # key=lambda x: int(x.split('/')[-1].split('.')[0]))
+        for img in images:
+            frame = cv2.imread(img)
+            yield frame
 
 
 def main():
@@ -18,7 +53,29 @@ def main():
     model.eval().to(device)
 
     # build tracker
-    tracker = build_tracker(model)
+    # tracker = build_tracker(model)
+
+    first_frame = True
+    root = "data/bag.avi"
+    video_name = root.split('/')[-1].split('.')[0]
+    cv2.namedWindow(video_name, cv2.WND_PROP_FULLSCREEN)
+
+    for frame in get_frames(root):
+        if first_frame:
+            try:
+                init_rect = cv2.selectROI(video_name, frame, False, False)
+            except:
+                exit()
+            # tracker.init(frame, init_rect)
+            first_frame = False
+        # else:
+        #     outputs = tracker.track(frame)
+        #     bbox = list(map(int, outputs['bbox']))
+        #     cv2.rectangle(frame, (bbox[0], bbox[1]),
+        #                   (bbox[0]+bbox[2], bbox[1]+bbox[3]),
+        #                   (0, 255, 0), 3)
+        #     cv2.imshow(video_name, frame)
+        #     cv2.waitKey(40)
 
 
 if __name__ == "__main__":
